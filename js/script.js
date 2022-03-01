7507a27254441511b6e11f796f9dac7d7f4a2c2c
Igor Stashok <glurmxm@gmail.com>
2022-03-01 19:38:51 +0100

    осталось доделать очистку поля

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
+//Открытие и закрытие попапа
 let popupСlose = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
 
 function showPopup() {
+  let popupItem = document.querySelector('.popup__item');
+      popupItem.value = '';
   popupСlose.classList.remove('popup_opened');
+
 }
 
 function hidePopup() {
   popupСlose.classList.add('popup_opened');
+
 }
 
 editButton.addEventListener('click', hidePopup);
 closeButton.addEventListener('click', showPopup);
 
-
-// Закрытие по клику   profile__edit-button
-
+//Очистка формы
 
