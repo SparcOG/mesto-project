018bf29af4b53e625b753c28a9c21d770e8a52a9
Igor Stashok <glurmxm@gmail.com>
2022-03-02 19:59:19 +0100

    Остановился на том, что конструкцию нужно сделать через IF

--- js/script.js
+++ js/script.js
@@ -1,35 +1,17 @@
 //Открытие и закрытие попапа, а также очиска формы при закрытии
-let popupСlose = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__close-button');
 
-let profile = document.querySelector('.profile__container');
-
-function showPopup() {
-  popupСlose.classList.remove('popup_opened');
-}
+let popupСlose = document.querySelector('.profile__edit-button');
 
 function hidePopup() {
-  let popupItemName = document.querySelector('.popup__item-name');
-  let popupItemProfession = document.querySelector('.popup__item-profession');
-
-    popupItemName.value = 'Жак-Ив Кусто';
-    popupItemProfession.value = 'Исследователь океана';
-
+    if (true) {
   popupСlose.classList.add('popup_opened');
+    } else {
+      popupСlose.classList.remove('popup_opened');
+    };
 }
 
-editButton.addEventListener('click', hidePopup);
-closeButton.addEventListener('click', showPopup);
+popupСlose.addEventListener('click', hidePopup);
 
-//Функция замены текста
- function addProfile () {
-  profile.insertAdjacentHTML('afterbegin', `
-    <h1 class="profile__title">Жак-Ив Кустоqqqq</h1>
-    <p class="profile__paragraph">Исследователь океанаqqqq</p>
-  `);
-}
 
-addProfile ();
 
 
