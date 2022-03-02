726db67ace28dd8bb215aed7ffe6ca31477de5e4
Igor Stashok <glurmxm@gmail.com>
2022-03-02 17:03:31 +0100

    Сделал чтобы имя и о себе были заполнеными

--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@ let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
 
 function showPopup() {
-  let popupItemName = document.querySelector('.popup__item-name');
-  let popupItemProfession = document.querySelector('.popup__item-profession');
-
-  popupItemName.value = '';
-  popupItemProfession.value = '';
-
   popupСlose.classList.remove('popup_opened');
 }
 
 function hidePopup() {
+  let popupItemName = document.querySelector('.popup__item-name');
+  let popupItemProfession = document.querySelector('.popup__item-profession');
+
+  popupItemName.value = 'Жак-Ив Кусто';
+  popupItemProfession.value = 'Исследователь океана';
+
   popupСlose.classList.add('popup_opened');
 }
 
