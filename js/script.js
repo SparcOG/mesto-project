c9c0a78f4d1ebaa099e1a86b74d7737dfd830b65
Igor Stashok <glurmxm@gmail.com>
2022-03-02 18:37:56 +0100

    Перед изменениями

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ let popupСlose = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
 
+let profile = document.querySelector('.profile__container');
+
 function showPopup() {
   popupСlose.classList.remove('popup_opened');
 }
@@ -11,13 +13,23 @@ function hidePopup() {
   let popupItemName = document.querySelector('.popup__item-name');
   let popupItemProfession = document.querySelector('.popup__item-profession');
 
-  popupItemName.value = 'Жак-Ив Кусто';
-  popupItemProfession.value = 'Исследователь океана';
+    popupItemName.value = 'Жак-Ив Кусто';
+    popupItemProfession.value = 'Исследователь океана';
 
   popupСlose.classList.add('popup_opened');
 }
 
 editButton.addEventListener('click', hidePopup);
 closeButton.addEventListener('click', showPopup);
+
+//Функция замены текста
+ function addProfile () {
+  profile.insertAdjacentHTML('afterbegin', `
+    <h1 class="profile__title">Жак-Ив Кустоqqqq</h1>
+    <p class="profile__paragraph">Исследователь океанаqqqq</p>
+  `);
+}
+
+addProfile ();
 
 
