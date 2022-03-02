dacb74b615cdc35d3dd2232d62b32675f72be334
Igor Stashok <glurmxm@gmail.com>
2022-03-02 16:19:37 +0100

    сделал чтобы форма удаляла введеный текст

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
-//Открытие и закрытие попапа
+//Открытие и закрытие попапа, а также очиска формы при закрытии
 let popupСlose = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
 
 function showPopup() {
-  let popupItem = document.querySelector('.popup__item');
-      popupItem.value = '';
-  popupСlose.classList.remove('popup_opened');
+  let popupItemName = document.querySelector('.popup__item-name');
+  let popupItemProfession = document.querySelector('.popup__item-profession');
 
+  popupItemName.value = '';
+  popupItemProfession.value = '';
+
+  popupСlose.classList.remove('popup_opened');
 }
 
 function hidePopup() {
   popupСlose.classList.add('popup_opened');
-
 }
 
 editButton.addEventListener('click', hidePopup);
 closeButton.addEventListener('click', showPopup);
 
-//Очистка формы
 
