35503ce52bfe124e439009c286bf81a652e255c2
Igor Stashok <glurmxm@gmail.com>
2022-03-17 17:02:14 +0100

    Теперь окно закрывется

--- js/script.js
+++ js/script.js
@@ -1,9 +1,75 @@
-//----------------------------Новая функциональность-----------------------------------------------------------------------------------
+//Открытие и закрытие popup
+const closeButton = document.querySelector('.popup__close-button');
+const popupHandlerButton = document.querySelector('.popup__handler-button');
+const profileInfo = document.querySelector('.profile__info');
+const popupСlose = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__container');
+
 //Открытие и закрытие popup-add-button
 const popupAddButton = document.querySelector('.popup-add-button');
 const popupAddButtonClose = document.querySelector('.popup-add-button__close-button');
+const popupAddButtonHandlerButton = document.querySelector('.popup-add-button__handler-button');
 const profileAddButton = document.querySelector('.profile__add-button');
 
+function showPopup() {
+  popupСlose.classList.remove('popup_opened');
+}
+  closeButton.addEventListener('click', showPopup);
+
+
+editButton.addEventListener('click', function (e) {
+  const popupItemName = document.querySelector('.popup__item-name');
+  const popupItemProfession = document.querySelector('.popup__item-profession');
+    popupItemName.value = 'Жак-Ив Кусто';
+    popupItemProfession.value = 'Исследователь океана';
+
+  if (e.target.classList.contains('profile__edit-button')) {
+    popupСlose.classList.add('popup_opened');
+  }
+});
+
+//Функция замены текста popup
+function addProfile(popupItemNameValue, popupItemProfessionValue) {
+
+  const profileContainer = document.querySelector('.profile__container');
+
+  const profileTitleRemove = document.querySelector('.profile__title');
+  const profileParagraphRemove = document.querySelector('.profile__paragraph');
+  const profileEditButtonRemove = document.querySelector('.profile__edit-button');
+
+  const profileTitle = document.createElement('h1');
+    profileTitle.classList.add('profile__title');
+    profileTitle.textContent = popupItemNameValue;
+
+  const profileParagraph = document.createElement('p');
+    profileParagraph.classList.add('profile__paragraph');
+    profileParagraph.textContent = popupItemProfessionValue;
+
+  const profileEditButton = document.createElement('button');
+    profileEditButton.classList.add('profile__edit-button');
+
+  profileContainer.append(profileTitle, profileParagraph);
+  profileContainer.append(profileEditButton);
+
+  popupСlose.classList.remove('popup_opened');
+
+  profileTitleRemove.remove();
+  profileParagraphRemove.remove();
+  profileEditButtonRemove.remove();
+};
+
+popupHandlerButton.addEventListener('click', function () {
+  const popupItemName = document.querySelector('.popup__item-name');
+  const popupItemProfession = document.querySelector('.popup__item-profession');
+
+  addProfile(popupItemName.value, popupItemProfession.value);
+
+  popupItemName.value = '';
+  popupItemProfession.value = '';
+});
+
+
+//----------------------------Новая функциональность-----------------------------------------------------------------------------------
 function showPopupAddButton() {
   popupAddButton.classList.remove('popup-add-button_opened');
 }
@@ -24,10 +90,6 @@ function addingСardToMarkup(galleryTitle, galleryImage) {
   galleryContainerClone.querySelector('.gallery__title').textContent = galleryTitle;
   galleryContainerClone.querySelector('.gallery__image').src = galleryImage;
 
-  //Добавление лайка к карточке новым елементам
-  galleryContainerClone.querySelector('.gallery__like-heart').addEventListener('click', function (evt) {
-    evt.target.classList.toggle('gallery__like-heart_active');
-  });
   //Удаление карточки нового елемента
   galleryContainerClone.querySelectorAll('.gallery__trash').forEach(galleryTrash => {
     galleryTrash.addEventListener('click', () => {
@@ -36,13 +98,12 @@ function addingСardToMarkup(galleryTitle, galleryImage) {
     });
   });
 
+  popupAddButton.classList.remove('popup-add-button_opened');
+
   gallery.prepend(galleryContainerClone);
 }
 
-const popupAddButtonHandlerButton = document.querySelector('.popup-add-button__handler-button');
-
 function closePopupHandlerButton() {
-  popupAddButton.classList.add('popup-add-button_opened');
   const galleryTitle = document.querySelector('.popup-add-button__item-name');
   const galleryImage = document.querySelector('.popup-add-button__item-image');
 
@@ -63,13 +124,14 @@ document.querySelectorAll('.gallery__like-heart').forEach(galleryLikeHeart => {
 
 //----------------------------Новая функциональность-------------------------------------------------------------------------
 //Удаление карточки
+const galleryTrash = document.querySelector('.gallery__trash');
+
 document.querySelectorAll('.gallery__trash').forEach(galleryTrash => {
-  galleryTrash.addEventListener('click', () => {
+  galleryTrash.addEventListener('click', (evt) => {
     const galleryCard = galleryTrash.closest('.gallery__card');
     galleryCard.remove();
   });
 });
-
 
 
 
