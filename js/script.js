124b6b2b5583c77b68df7a6edc9db95b467ae16e
Igor Stashok <glurmxm@gmail.com>
2022-03-20 21:00:12 +0100

    нужно разобраться почему карточки не добавились

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const closeButton = document.querySelector('.popup__close-button');
 const popupHandlerButton = document.querySelector('.popup__handler-button');
 const profileInfo = document.querySelector('.profile__info');
 const popupСlose = document.querySelector('.popup');
+
+//Слушаем клик
 const editButton = document.querySelector('.profile__container');
 
 //Открытие и закрытие popup-add-button
@@ -10,6 +12,11 @@ const popupAddButton = document.querySelector('.popup-add-button');
 const popupAddButtonClose = document.querySelector('.popup-add-button__close-button');
 const popupAddButtonHandlerButton = document.querySelector('.popup-add-button__handler-button');
 const profileAddButton = document.querySelector('.profile__add-button');
+
+//Добавление карточки
+const galleryContainerTemplate = document.querySelector('.gallery__container-template');
+const gallery = document.querySelector('.gallery');
+
 
 function showPopup() {
   popupСlose.classList.remove('popup_opened');
@@ -84,13 +91,17 @@ profileAddButton.addEventListener('click', closePopupAddButton);
 //Добавление карточки
 function addingСardToMarkup(galleryTitle, galleryImage) {
   const gallery = document.querySelector('.gallery');
-  const galleryContainerTemplate = document.querySelector('#gallery__container-template');
   const galleryContainerClone = galleryContainerTemplate.content.cloneNode(true);
 
   galleryContainerClone.querySelector('.gallery__title').textContent = galleryTitle;
   galleryContainerClone.querySelector('.gallery__image').src = galleryImage;
 
-  //Удаление карточки нового елемента
+//Добавление лайка к карточке
+  galleryContainerClone.querySelector('.gallery__like-heart').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('gallery__like-heart_active');
+  });
+
+  //Удаление карточки новоcозданого елемента
   galleryContainerClone.querySelectorAll('.gallery__trash').forEach(galleryTrash => {
     galleryTrash.addEventListener('click', () => {
       const galleryCard = galleryTrash.closest('.gallery__card');
@@ -124,16 +135,50 @@ document.querySelectorAll('.gallery__like-heart').forEach(galleryLikeHeart => {
 
 //----------------------------Новая функциональность-------------------------------------------------------------------------
 //Удаление карточки
-const galleryTrash = document.querySelector('.gallery__trash');
-
 document.querySelectorAll('.gallery__trash').forEach(galleryTrash => {
-  galleryTrash.addEventListener('click', (evt) => {
+  galleryTrash.addEventListener('click', () => {
     const galleryCard = galleryTrash.closest('.gallery__card');
     galleryCard.remove();
   });
 });
 
+//----------------------------Новая функциональность-------------------------------------------------------------------------
+//Добавление массива карточек в DOM
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+  ];
 
+  initialCards.forEach(function (card) {
+    const clonСards = galleryContainerTemplate.cloneNode(true);
+
+    clonСards.querySelector('.gallery__title').textContent = card.name;
+    clonСards.querySelector('.gallery__image').src = card.link;
+
+    gallery.append(clonСards)
+  })
 
 
 
